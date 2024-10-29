455ebd64ab6069a5dc27e9b3e7d4f0a41c76bb5f
Arturo Batista <batistaquiroz@gmail.com>
2024-10-29 11:21:34 -0500

    Logout functionally done on the client-side

--- frontend/src/components/ChatContainer/ChatContainer.tsx
+++ frontend/src/components/ChatContainer/ChatContainer.tsx
@@ -1,14 +1,40 @@
-import React from "react";
 import styles from "./chat_container.module.scss";
 import UserBox from "../UserBox/UserBox";
 import Chat from "../Conversation/Conversation";
+import logOutIcon from "../../assets/icons/logout.png";
+import axios from "axios";
+import { useAuthContext } from "../../context/AuthContext";
 
 export default function ChatContainer() {
+  const { setAuthUser } = useAuthContext();
+
+  async function handleLougOut() {
+    try {
+      await axios.post("/api/auth/logout");
+      setAuthUser(null);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        alert(error.response.data.error);
+      } else {
+        alert("An unexpected error ocurred, please try again later.");
+        console.log(error);
+      }
+    } finally {
+      setAuthUser(null);
+    }
+  }
+
   return (
     <div className={styles.ChatContainer}>
       <div className={styles.UserListSection}>
         <div className={styles.TopSection}>
-          <h2>List of Users</h2>
+          <div>
+            <button className={styles.LogOutButton} onClick={handleLougOut}>
+              <img src={logOutIcon} alt="Log out Icon" />
+            </button>
+            <h2>Users list</h2>
+          </div>
+
           <label className={styles.Search}>
             Search: <input type="text" placeholder="Look for.." />
           </label>
@@ -19,12 +45,9 @@ export default function ChatContainer() {
           <UserBox />
           <UserBox />
           <UserBox />
-          <UserBox />
-          <UserBox />
-          <UserBox />
         </div>
       </div>
-      <div className={styles.ChattingSection}> 
+      <div className={styles.ChattingSection}>
         <Chat />
       </div>
     </div>
